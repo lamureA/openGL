9f403f5aafa02d4894a86f725d9e3ea1e7b91010
Alexandre Lamure <alexandre.lamure@epita.fr>
2019-05-11 12:44:47 +0200

    EDT - init of glut, glew and openGL

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
 #include <GL/glut.h>
 
 void renderScene(void) {
@@ -14,14 +16,43 @@ void renderScene(void) {
     glutSwapBuffers();
 }
 
+void init_glut(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitContextVersion(3, 0);
+    glutInitContextProfile(GLUT_CORE_PROFILE);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100,100);
+    glutInitWindowSize(400,400);
+    glutCreateWindow("OpenGL - TP1");
+    //glutFullScreen();
+}
+
+bool init_glew() {
+    GLenum init = glewInit();
+    if (init != GLEW_OK)
+    {
+        std::cerr << "GlewInit fails: " << glewGetErrorString(init) << std::endl;
+        return false;
+    }
+    return true;
+}
+
+void init_gl() {
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+    glDepthRange(0.0, 1.0);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CCW);
+    glClearColor(0, 0, 0, 0);
+}
+
 
 int main(int argc, char **argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowPosition(100,100);
-    glutInitWindowSize(320,320);
-    glutCreateWindow("Lighthouse3D- GLUT Tutorial");
-    //glutFullScreen();
+    init_glut(argc, argv);
+    if (!init_glew())
+        return 1;
+    init_gl();
 
     // register callbacks
     glutDisplayFunc(renderScene);
@@ -30,5 +61,8 @@ int main(int argc, char **argv) {
     glutMainLoop();
 
     return 0;
+
+
+
 }
 
